01fb199e7cbe31e5af70f230d92aecf2fbf1f509
chayuearn-00 <chayuearn@gmail.com>
2026-03-13 12:43:20 +0700

    change font, create LogCard file.

---
+++ frontend/src/components/LogCard.tsx
@@ -0,0 +1,21 @@
+export default function LogTable () {
+    return (
+        <div className="card w-full h-full">
+            <table className="table-fixed w-full">
+                <thead>
+                    <tr className="font-bold text-2xl">
+                        <th className="">No.</th>
+                        <th className="w-1/4">Name</th>
+                        <th>Zone</th>
+                        <th>Station</th>
+                        <th>Location</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+    )
+}
